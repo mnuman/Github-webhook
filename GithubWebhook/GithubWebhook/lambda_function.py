4d7e39fc67818aa70296bf56618d248e620ab848
Milco Numan <milco.numan@gmail.com>
2018-02-24 21:32:35 +0000

    Messed up text markers ...

--- GithubWebhook/GithubWebhook/lambda_function.py
+++ GithubWebhook/GithubWebhook/lambda_function.py
@@ -1,8 +1,24 @@
 import json
+import os
+import tweepy
+
+# importing the environment variables - is this safe? We'll see ...
+consumer_key = os.environ['CONSUMER_KEY']
+consumer_secret = os.environ['CONSUMER_SECRET']
+access_token = os.environ['ACCESS_TOKEN']
+access_token_secret = os.environ['ACCESS_TOKEN_SECRET']
+
+# setup authentication with twitter
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
 def lambda_handler(event, context):
-    # Just print the event as a first tracer bullet
-    print(json.dumps(event, indent=2))
-    response_body =  { "message": 'Howdy from Lambda!'}
+    mytweet = f"Hi, {event['body']['commit'][0]['committer']['name']} just commited with message {event['body']['commit'][0]['message']}!"
+    status = api.update_status(status=mytweet)
+    
+    response_body =  { "message": "Your tweet has been posted!"}
     
     return {
       "isBase64Encoded": "false",
